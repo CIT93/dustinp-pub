01ebb9802349dca3f357a93a96a1c16e9f722ac8
Dustin Petersen <d86petersen@gmail.com>
2022-08-09 14:49:02 -0700

    added stylesheet

---
+++ w2/app.js
@@ -0,0 +1,40 @@
+// Display on page function
+const showOnPage = function (text) {
+    let newParagraph = document.createElement('p')
+    newParagraph.innerHTML = text
+    let outputDiv = document.getElementById('output')
+    outputDiv.append(newParagraph)
+  }
+  
+  // Most common decision that I make every day is to check the temp and decide what I should wear based on that information.
+
+  // Global Variables
+   let temp = 100;
+   let isWearHoodie = false;
+    let isWearShorts = false;
+    let isWearTankTop = false;
+    let isWearTshirt = false;
+    let isWearPants = false;
+// if else statements
+    if (temp >= 90 && temp <= 120) {
+      let  isWearShorts = true;
+        isWearTankTop = true;
+
+    showOnPage('You should wear shorts and a tank top.');
+    } else if (temp >= 70 && temp <= 89) {
+        isWearShorts = true;
+        isWearTshirt = true;
+
+    showOnPage('You should wear shorts and a t-shirt.');
+    } else if (temp <= 69 && temp >= 40) {
+        isWearPants = true;
+        isWearTshirt = true;
+        isWearHoodie = true;
+    showOnPage('You should wear a hoodie, pants and a t-shirt.');
+    } else {
+        showOnPage('You should just stay inside.');
+    }
+
+
+    
+    
